649b35c887c0e40cd1762e0d4b691768b0cd789a
tyjcalvert <tyjcalvert@gmail.com>
2025-01-22 16:01:34 -0500

    added server.py, updated test

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+from EmotionDetection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emo_detector():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    
+
