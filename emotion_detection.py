8ef257abc25b0bb846ddd41862d1dceb8e920aff
tyjcalvert <tyjcalvert@gmail.com>
2025-01-22 14:51:38 -0500

    updated emo detection file

---
+++ emotion_detection.py
@@ -0,0 +1,13 @@
+import requests, json
+
+def emotion_detector(text_to_analyse):
+    try:
+        url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+        headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+        myobj = { "raw_document": { "text": text_to_analyse } }
+        response = requests.post(url, json = myobj, headers = headers)
+        formatted_response = json.loads(response.json)
+        return formatted_response
+    except Exception:
+        return { "error": Exception }
+        
